61f1424a55c7c72856c741adc76090c636737c89
aron <aron.anders@tum.de>
2024-12-13 07:49:47 +0100

    pls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef, FormEvent, ChangeEvent } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+//import { v4 as uuidv4 } from 'uuid'
 import { useRouter } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
 
